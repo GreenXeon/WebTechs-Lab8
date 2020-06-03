57aae835fefe36a9675538cbabeb1c226630c739
Zakhar Shyshkin <zahar.shishkin.2001@mail.ru>
2020-06-03 13:26:32 +0300

    os analytics added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Lab 7</title>
+	<title>Lab 8</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
@@ -49,6 +49,8 @@
 					}
 				}
 				stat_generator();
+
+
 				function user_write($users){
 					$f = fopen("users.txt", 'a'); 
 					$a = explode(';', $users);
@@ -71,10 +73,80 @@
 				}
 
 				function stat_generator(){
-					var_dump($_SERVER['HTTP_USER_AGENT']);
+					$host = 'localhost';
+					$database = 'statistics';
+					$user = 'root';
+					$password = 'root';
+					$link = mysqli_connect($host, $user, $password, $database) 
+    					or die("Ошибка " . mysqli_error($link));
+
+					$os = php_uname('v');
+					$pattern = '/\(.+\)/';
+					preg_match($pattern, $os, $match);
+					$os = trim($match[0], '(');
+					$os = trim($os, ')');
+					echo $os;
+
+					$query ="SELECT * FROM os WHERE name = '$os'";
+					$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+					if($result)
+					{
+						$rows = mysqli_num_rows($result);
+						//echo "Successful request.";					    
+					}
+
+					if ($rows == 0){
+						$query = "INSERT INTO `os`(`key`, `name`, `count`) VALUES (NULL, '$os', 1)";
+						$result = mysqli_query($link, $query) or die ("Error!");
+						//echo "Creation.". '<br>';
+					}
+					else{
+						$row = mysqli_fetch_row($result);
+						$count = $row[2] + 1;
+						$query = "UPDATE `os` SET `count`= '$count'";
+						$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+						if($result){
+							//echo "Ok";
+						}
+			?>
+			<div class="info">
+			<?php
+						$query ="SELECT * FROM os";
+						$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+
+						echo "<table><tr><th>Id</th><th>OS</th><th>Количество</th></tr>";
+
+						for ($i = 0 ; $i < $rows ; ++$i)
+					    {
+					        $row = mysqli_fetch_row($result);
+					        echo "<tr>";
+					            for ($j = 0 ; $j < 3 ; ++$j) echo "<td>$row[$j]</td>";
+					        echo "</tr>";
+					    }
+					    echo "</table>";
+					     
+					    // очищаем результат
+					    mysqli_free_result($result);
+					}
+					mysqli_close($link);
 				}
+
+				function getRealIpAddr() {
+				  if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+				  	$ip=$_SERVER['HTTP_CLIENT_IP']; 
+				  }				  	
+				  elseif 
+				  	(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) 
+				  { 
+				  	$ip=$_SERVER['HTTP_X_FORWARDED_FOR']; 
+				  }
+				  else { 
+				  	$ip=$_SERVER['REMOTE_ADDR']; 
+				  }
+				  return $ip;
+				}	
 			?>	
-			
+			</div>
 			<form action="" method="POST">
 				<span>To</span><br>
 				<input class = "fields" required type="text" name="users" value = "<?php echo $users; ?>"><br>
